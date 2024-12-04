9fd7732720103d95ed55b12201f46d112a52f081
Simon Ricard <ricardsimon@hotmail.com>
2024-12-03 23:35:11 -0500

    move test input to module attribute

--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,34 +3,23 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  test "part1" do
-    input =
-      """
-      3   4
-      4   3
-      2   5
-      1   3
-      3   9
-      3   3
-      """
+  @input """
+  3   4
+  4   3
+  2   5
+  1   3
+  3   9
+  3   3
+  """
 
-    result = part1(input)
+  test "part1" do
+    result = part1(@input)
 
     assert result == 11
   end
 
   test "part2" do
-    input =
-      """
-      3   4
-      4   3
-      2   5
-      1   3
-      3   9
-      3   3
-      """
-
-    result = part2(input)
+    result = part2(@input)
 
     assert result == 31
   end
