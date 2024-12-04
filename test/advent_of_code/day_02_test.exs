185723c618076ebf8ebfaf01fb9adc8660a72581
Simon Ricard <ricardsimon@hotmail.com>
2024-12-03 23:35:17 -0500

    day2

--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,18 +3,25 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
+  @input """
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
+  """
+
   @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    result = part1(@input)
 
-    assert result
+    assert result == 2
   end
 
   @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
     assert result
   end
